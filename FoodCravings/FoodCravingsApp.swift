1f1ec0d392788c1b35f068fa04c42bc629b3680d
Lychnobite1 <muratbaser841@gmail.com>
2023-10-03 16:25:42 +0300

    Initial Commit

---
+++ FoodCravings/FoodCravingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodCravingsApp.swift
+//  FoodCravings
+//
+//  Created by MURAT BAŞER on 3.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodCravingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
